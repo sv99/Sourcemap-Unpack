1fb40c8588ea892703495173ed58aa36685e42bd
S.Volkov <sv99@inbox.ru>
2025-06-17 12:16:15 +0300

    Add --double param for saving dublicates files

--- unmap.js
+++ unmap.js
@@ -3,6 +3,7 @@
 import fs from "fs-extra";
 import filenamify from "filenamify";
 import os from "os";
+import { existsSync } from "fs"
 import { resolve } from "path";
 import { Command } from "commander";
 const program = new Command();
@@ -13,6 +14,7 @@ const opts = program
   .requiredOption("-p, --path <p>", "Input source map file")
   .option("-f, --filter <f>", "Filter out file names")
   .option("-o, --output <o>", "Output folder", "./")
+  .option("-d, --double", "Save doublicates")
   .parse(process.argv)
   .opts();
 
@@ -32,12 +34,31 @@ fs.readFile(opts.path)
     const files = js.sources
       .filter((path) => (opts.filter ? path.includes(opts.filter) : true))
       .map((path, idx) => {
-        const outpath = resolve(
+        let outpath = resolve(
           opts.output,
           isWindows ? filenamify(path) : path
         );
+        if (opts.double) {
+          outpath = checkDouble(outpath)
+        }
         return fs.outputFile(outpath, js.sourcesContent[idx]);
       });
     return Promise.all(files);
   })
   .then(() => console.log(`${opts.path} unpacked`));
+
+function checkDouble(filePath) {
+if (!existsSync(filePath)) {
+    return filePath
+  } else {
+    // add suffix to the file name
+    let suffix = 1
+    let filePathSuffix = filePath + `.${suffix}`
+    while (existsSync(filePathSuffix)) {
+      suffix++
+      filePathSuffix = filePath + `.${suffix}`
+    }
+    console.error(`Suffix file: ${filePathSuffix}`)
+    return filePathSuffix
+  }
+}
